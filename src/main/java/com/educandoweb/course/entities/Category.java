a021602179a994e3f6e5c110bf557687a13f748c
Lucas Kenzo <kenzoawane@gmail.com>
2023-02-21 18:29:19 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,9 +1,30 @@
 package com.educandoweb.course.entities;
 
-public class Category {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "tb_category")
+public class Category  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {}
 
@@ -29,5 +50,27 @@ public class Category {
 		this.name = name;
 	}
 
-	 
+	public Set<Product> getProducts(){
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
 }
