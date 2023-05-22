a0286a7d8df42e0826a1b070d2604693fc33a723
Lucas Kenzo <kenzoawane@gmail.com>
2023-05-21 23:10:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/config/TesteConfig.java
+++ src/main/java/com/educandoweb/course/config/TesteConfig.java
@@ -62,7 +62,7 @@ public class TesteConfig implements CommandLineRunner{
 		
 		categoryRepsitory.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		//productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		p1.getCaterories().add(cat2);
 		p2.getCaterories().add(cat1);
